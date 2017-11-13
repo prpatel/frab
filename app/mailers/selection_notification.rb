76120e355c671754bc885b38cb463662421d105e
pratik patel <pratik@mypatelspace.com>
2017-11-13 10:01:46 -0500

    added usage of conference.email rather than default email for selection emaiuls

--- app/mailers/selection_notification.rb
+++ app/mailers/selection_notification.rb
@@ -15,6 +15,7 @@ class SelectionNotification < ActionMailer::Base
       @from = Settings['from_email']
     end
     mail(
+      from: @from,
       reply_to: @conference.email,
       to: event_person.person.email,
       subject: @notification.accept_subject.gsub('%{conference}', @conference.title).gsub('%{event}', @event.title).gsub('%{forename}', @person.first_name).gsub('%{surname}', @person.last_name).gsub('%{public_name}', @person.public_name), locale: @locale, title: @conference.title
@@ -30,6 +31,7 @@ class SelectionNotification < ActionMailer::Base
     raise "Notification for #{@locale} not found" if @notification.nil?
 
     mail(
+      from: @from,
       reply_to: @conference.email,
       to: @person.email,
       subject: @notification.accept_subject.gsub('%{conference}', @conference.title).gsub('%{event}', @event.title).gsub('%{forename}', @person.first_name).gsub('%{surname}', @person.last_name).gsub('%{public_name}', @person.public_name), locale: @locale, title: @conference.title
